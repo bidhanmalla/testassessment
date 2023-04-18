78e2fef0721bfdc2589467e3a0b229f48f5b9aef
bidhanmalla <127164867+bidhanmalla@users.noreply.github.com>
2023-04-18 19:35:16 +0930

    Create models.py

---
+++ django/models.py
@@ -0,0 +1,6 @@
+class Item:
+    def __init__(self, name, description, price, image):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.image = image
