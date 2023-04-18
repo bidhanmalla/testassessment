0d32ead7dbce1428ba2263800364ade8eb3f6883
bidhanmalla <127164867+bidhanmalla@users.noreply.github.com>
2023-04-18 19:51:08 +0930

    Update manage.py

--- django/manage.py
+++ django/manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import sys
+sys.path.append("/path/to/myproject")
 
 def main():
     """Run administrative tasks."""
